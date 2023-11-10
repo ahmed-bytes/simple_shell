749381cc1f7f5ec0883f1565798e03d070d68a3f
Valentine Nyibiam <nyibiamsaanmoiyol@gmail.com>
2023-11-10 07:03:39 -0800

    Add a function that tokenizes the user input

--- tokenize_input.c
+++ tokenize_input.c
@@ -7,27 +7,39 @@
  *
  * Return: An array of the tokens
  */
-char *tokenize_input(char *input, char *delim)
+char **tokenize_input(char *input, char *delim)
 {
-	char *input_copy, *token_array, *token;
+	char *input_copy, **token_array, *token;
 	size_t token_count = 0, i = 0;
-
-	input_copy = strdup(input);
+	
+	input_copy = malloc(sizeof(char) * (strlen(input) + 1));
+	if(input_copy == NULL)
+	{
+		perror("malloc failed");
+		exit(EXIT_FAILURE);
+	}
+	strcpy(input_copy, input);
 
 	token = strtok(input_copy, delim);
 	while(token != NULL)
 	{
-		token = strtok(NULL, delim);
 		token_count++;
+		token = strtok(NULL, delim);
 	}
 	free(input_copy);
 
-	token_array = malloc((sizeof(char *) * token_count) + 1);
+	token_array = malloc(sizeof(char *) * (token_count + 1));
 	token = strtok(input, delim);
 	while(token != NULL)
 	{
+		token_array[i] = malloc(sizeof(char) * (strlen(token) + 1));
+		if(token_array[i] == NULL)
+		{
+			perror("malloc failed");
+			exit(EXIT_FAILURE);
+		}
+		strcpy(token_array[i], token);
 		token  = strtok(NULL, delim);
-		token_array[i] = token;
 		i++;
 	}
 	token_array[i] = NULL;
